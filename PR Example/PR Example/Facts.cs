872022ceb2f27fd5a6f91b1ddeb5abe97d2e8058
Gentian Gashi <genti_gashi8@hotmail.com>
2019-12-25 00:10:07 +0000

    Add files via upload

---
+++ PR Example/PR Example/Facts.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PR_Example
+{
+    class Facts
+    {
+        bool Headache;
+        bool Vomiting;
+        bool Cramp;
+        bool Rash;
+        bool Cough;
+        bool Fatigue;
+        bool Nausea;
+        bool Dizziness;
+        bool Cold;
+        bool ChestPain;
+        bool Infection;
+        bool Flu;
+        bool Healthy;
+        bool PrescribeAntibiotics;
+        bool PrescribePainkillers;
+        bool Gastroenteritis;
+
+        public Facts()
+        {
+            Headache = false;
+            Vomiting = false;
+            Cramp = false;
+            Rash = false;
+            Cough = false;
+            Fatigue = false;
+            Nausea = false;
+            Dizziness = false;
+            Cold = false;
+            ChestPain = false;
+            Infection = false;
+            Flu = false;
+            Healthy = false;
+            PrescribeAntibiotics = false;
+            PrescribePainkillers = false;
+            Gastroenteritis = false;
+        }
+
+        public Facts(bool head, bool vomit, bool cram, bool ras, bool coug, bool fatig, bool nause, bool dizzi, bool col, bool chest, bool infect, bool fl, bool health, bool antibio, bool pain, bool gast)
+        {
+            Headache = head;
+            Vomiting = vomit;
+            Cramp = cram;
+            Rash = ras;
+            Cough = coug;
+            Fatigue = fatig;
+            Nausea = nause;
+            Dizziness = dizzi;
+            Cold = col;
+            ChestPain = chest;
+            Infection = infect;
+            Flu = fl;
+            Healthy = health;
+            PrescribeAntibiotics = antibio;
+            PrescribePainkillers = pain;
+            Gastroenteritis = gast;
+        }
+        public bool headache
+        { get { return Headache; } set { } }
+
+        public bool vomiting
+        { get { return Vomiting; } set { } }
+
+        public bool cramp
+        { get { return Cramp; } set { } }
+
+        public bool rash
+        { get { return Rash; } set { } }
+
+        public bool cough
+        { get { return Cough; } set { } }
+
+        public bool fatigue
+        { get { return Fatigue; } set { } }
+
+        public bool nausea
+        { get { return Nausea; } set { } }
+
+        public bool dizziness
+        { get { return Dizziness; } set { } }
+
+        public bool cold
+        { get { return Cold; } set { } }
+
+        public bool chestPain
+        { get { return ChestPain; } set { } }
+
+        public bool infection
+        { get { return Infection; } set { } }
+
+        public bool flu
+        { get { return Flu; } set { } }
+
+        public bool healthy
+        { get { return Healthy; } set { } }
+
+        public bool prescribeAntibiotics
+        { get { return PrescribeAntibiotics; } set { } }
+
+        public bool prescribePainkillers
+        { get { return prescribePainkillers; } set { } }
+
+        public bool gastroenteritis
+        { get { return Gastroenteritis; } set { } }
+
+    }
+}
---
+++ PR Example/PR Example/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace PR_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        int numberOfFacts = 0;
+        Facts FACTS = new Facts();
+
+        void addFACTS()
+        {
+
+            FACTS.headache = false;
+            FACTS.vomiting = false;
+            FACTS.cramp = false;
+            FACTS.rash = false;
+            FACTS.cough = false;
+            FACTS.fatigue = false;
+            FACTS.nausea = false;
+            FACTS.dizziness = false;
+            FACTS.cold = false;
+            FACTS.chestPain = false;
+            FACTS.infection = false;
+            FACTS.flu = false;
+            FACTS.healthy = false;
+            FACTS.prescribeAntibiotics = false;
+            FACTS.prescribePainkillers = false;
+            FACTS.gastroenteritis = false;
+        }
+
+        private void countFACTS()
+        {
+            int count = 0;
+            foreach (fact in FACTS)
+            {
+                if (FACTS[fact]) count++;
+            }
+            return count;
+        }
+
+        void printFACTS()
+        {
+            foreach (fact in FACTS)
+            {
+                document.write("<p>" + fact + " is " + FACTS[fact] + "<p>");
+            }
+        }
+
+        private void infer()
+        {
+            do
+            {
+                numberOfFacts = countFACTS();
+                (FACTS.headache && FACTS.vomiting) ? FACTS.flu = true : FACTS.flu = false;
+                FACTS.infection? FACTS.prescribeAntibiotics = true : FACTS.prescribeAntibiotics = false;
+                FACTS.headache? FACTS.prescribePainkillers = true : FACTS.prescribePainkillers = false;
+                (FACTS.nausea && FACTS.fatigue) ? FACTS.gastroenteritis = true : FACTS.gastroenteritis = false;
+                FACTS.infection? FACTS.healthy = true : FACTS.healthy = false;
+                FACTS.prescribePainkillers? FACTS.prescribePainkillers = true : FACTS.prescribePainkillers = false;
+            }
+            while (numberOfFacts != countFACTS())
+        }
+
+            addFACTS();
+            FACTS.headache = true;
+            FACTS.vomiting = true;
+            infer();
+            ntFACTS();
+    }
+}
